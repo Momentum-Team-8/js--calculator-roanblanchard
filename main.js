94b5bee21d94192a8666464f94d636deecc123fc
roanblanchard <roan.g.blanchard@icloud.com>
2021-05-19 19:21:54 -0400

    fixed equals sign printing

--- main.js
+++ main.js
@@ -4,16 +4,121 @@ let newElement = document.createElement('h1')
 
 
 
-document.addEventListener('click', function(event){
+
+
+document.querySelector('#equals').addEventListener('click', function (event) {
+    // output = ""
+    let equals = document.createTextNode(eval(output.innerText))
+    document.getElementById("output").innerHTML = ""
+    newElement.appendChild(equals)
+    output.appendChild(newElement)
+})
+
+
+
+
+
+document.querySelector('#one').addEventListener('click', function (event){
     let newEntry = document.createTextNode(event.target.innerHTML)
     newElement.appendChild(newEntry)
     output.appendChild(newElement)
-  })
+    console.log(event.target.innerHTML)
+})
 
-  document.querySelector('#equals').addEventListener('click', function (event) {
-    // output = ""
-    console.log(eval(output.innerText))
-    let equals = document.createTextNode(eval(output.innerText))
-    newElement.appendChild(equals)
+document.querySelector('#two').addEventListener('click', function (event){
+    let newEntry = document.createTextNode(event.target.innerHTML)
+    newElement.appendChild(newEntry)
     output.appendChild(newElement)
-  })
+    console.log(event.target.innerHTML)
+})
+
+document.querySelector('#three').addEventListener('click', function (event){
+    let newEntry = document.createTextNode(event.target.innerHTML)
+    newElement.appendChild(newEntry)
+    output.appendChild(newElement)
+    console.log(event.target.innerHTML)
+})
+
+document.querySelector('#four').addEventListener('click', function (event){
+    let newEntry = document.createTextNode(event.target.innerHTML)
+    newElement.appendChild(newEntry)
+    output.appendChild(newElement)
+    console.log(event.target.innerHTML)
+})
+
+document.querySelector('#five').addEventListener('click', function (event){
+    let newEntry = document.createTextNode(event.target.innerHTML)
+    newElement.appendChild(newEntry)
+    output.appendChild(newElement)
+    console.log(event.target.innerHTML)
+})
+
+document.querySelector('#six').addEventListener('click', function (event){
+    let newEntry = document.createTextNode(event.target.innerHTML)
+    newElement.appendChild(newEntry)
+    output.appendChild(newElement)
+    console.log(event.target.innerHTML)
+})
+
+document.querySelector('#seven').addEventListener('click', function (event){
+    let newEntry = document.createTextNode(event.target.innerHTML)
+    newElement.appendChild(newEntry)
+    output.appendChild(newElement)
+    console.log(event.target.innerHTML)
+})
+
+document.querySelector('#eight').addEventListener('click', function (event){
+    let newEntry = document.createTextNode(event.target.innerHTML)
+    newElement.appendChild(newEntry)
+    output.appendChild(newElement)
+    console.log(event.target.innerHTML)
+})
+
+document.querySelector('#nine').addEventListener('click', function (event){
+    let newEntry = document.createTextNode(event.target.innerHTML)
+    newElement.appendChild(newEntry)
+    output.appendChild(newElement)
+    console.log(event.target.innerHTML)
+})
+
+document.querySelector('#point').addEventListener('click', function (event){
+    let newEntry = document.createTextNode(event.target.innerHTML)
+    newElement.appendChild(newEntry)
+    output.appendChild(newElement)
+    console.log(event.target.innerHTML)
+})
+
+document.querySelector('#add').addEventListener('click', function (event){
+    let newEntry = document.createTextNode(event.target.innerHTML)
+    newElement.appendChild(newEntry)
+    output.appendChild(newElement)
+    console.log(event.target.innerHTML)
+})
+
+document.querySelector('#divide').addEventListener('click', function (event){
+    let newEntry = document.createTextNode(event.target.innerHTML)
+    newElement.appendChild(newEntry)
+    output.appendChild(newElement)
+    console.log(event.target.innerHTML)
+})
+
+document.querySelector('#multi').addEventListener('click', function (event){
+    let newEntry = document.createTextNode(event.target.innerHTML)
+    newElement.appendChild(newEntry)
+    output.appendChild(newElement)
+    console.log(event.target.innerHTML)
+})
+
+document.querySelector('#zero').addEventListener('click', function (event){
+    let newEntry = document.createTextNode(event.target.innerHTML)
+    newElement.appendChild(newEntry)
+    output.appendChild(newElement)
+    console.log(event.target.innerHTML)
+})
+
+document.querySelector('#sub').addEventListener('click', function (event){
+    let newEntry = document.createTextNode(event.target.innerHTML)
+    newElement.appendChild(newEntry)
+    output.appendChild(newElement)
+    console.log(event.target.innerHTML)
+})
